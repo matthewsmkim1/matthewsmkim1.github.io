1a9e85ce85ed7b1fe82587f6dc24106e941ea69f
Matt <Matthew.sm.kim1@gmail.com>
2017-08-10 11:12:01 -0400

    form

--- index.php
+++ index.php
@@ -192,16 +192,40 @@
 
        <section id="slide-3" class="homeSlide">
          <div class = "bcg">
-            <form  action="" method="POST" enctype="multipart/form-data">
-               <input type="hidden" name="action" value="submit">
-                  <div id = "whiteFont">Your name:<br> </div>
-               <input name="name" type="text" value="" size="30"/><br>
-                  <div id = "whiteFont"> Your email:<br> </div>
-               <input name="email" type="text" value="" size="30"/><br>
-                  <div id = "whiteFont"> Your message:<br> </div>
-               <textarea name="message" rows="7" cols="30"></textarea><br>
-               <input type="submit" value="Send email"/>
-            </form>
+            <?php
+            $action=$_REQUEST['action'];
+            if ($action=="")    /* display the contact form */
+                {
+                ?>
+                <form  action="" method="POST" enctype="multipart/form-data">
+                  <input type="hidden" name="action" value="submit">
+                      <div id = "whiteFont">Your name:<br> </div>
+                  <input name="name" type="text" value="" size="30"/><br>
+                      <div id = "whiteFont"> Your email:<br> </div>
+                  <input name="email" type="text" value="" size="30"/><br>
+                      <div id = "whiteFont"> Your message:<br> </div>
+                  <textarea name="message" rows="7" cols="30"></textarea><br>
+                  <input type="submit" value="Send email"/>
+               </form>
+                <?php
+                }
+            else                /* send the submitted data */
+                {
+                $name=$_REQUEST['name'];
+                $email=$_REQUEST['email'];
+                $message=$_REQUEST['message'];
+                if (($name=="")||($email=="")||($message==""))
+                    {
+                    echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+                    }
+                else{
+                    $from="From: $name<$email>\r\nReturn-path: $email";
+                    $subject="Message sent using your contact form";
+                    mail("matthew.sm.kim1@gmail.com", $subject, $message, $from);
+                    echo "Email sent!";
+                    }
+                }
+            ?>
          </div>
        </section>
 
