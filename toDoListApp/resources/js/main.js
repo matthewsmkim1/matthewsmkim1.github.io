f40ba9c9596d09bec7ac3917144c6cb316fd7319
Matt <Matthew.sm.kim1@gmail.com>
2017-12-10 18:12:16 -0500

    added home buttn for toDoListApp

--- toDoListApp/resources/js/main.js
+++ toDoListApp/resources/js/main.js
@@ -76,7 +76,7 @@ function removeItem() {
 function completeItem() {
     var item = this.parentNode.parentNode;
     var parent = item.parentNode;
-    
+
     var id = parent.id;
     var value = item.innerText;
 
