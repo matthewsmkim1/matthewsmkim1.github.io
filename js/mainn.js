1ca8ad18d80b9ae52b6cb0f5e8c2e92739b0fbf1
Matt <Matthew.sm.kim1@gmail.com>
2018-01-18 22:53:28 -0500

    created and fixed abtme page

--- js/mainn.js
+++ js/mainn.js
@@ -10,11 +10,22 @@ $(document).ready(function(){
 
 		e.preventDefault();
 	});
-	/*
+
+
+/*
+
    $('.fa-facebook').hover(function() {
-   	$(this).toggleClass('fa-spin');
+		//does not find .fa-stack class for some reason
+		var col = document.getElementsByClassName(".fa-stack");
+		for(i=0; i<col.length; i++) {
+    		col[i].style.color = 'black';
+  		}
    });
+
 */
+
+
+
 });
 
 ( function( $) {
