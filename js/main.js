6a268d6d9217c2ba92f0c8e1a758906f77de007c
Matt <Matthew.sm.kim1@gmail.com>
2018-03-07 14:08:51 -0500

    added nav bar

--- js/main.js
+++ js/main.js
@@ -7,8 +7,40 @@ $(document).ready(function(){
 
 	    $('html, body').stop().animate({
 	        'scrollTop': $target.offset().top
-	    }, 900, 'swing', function () {
+	    }, 950, 'swing', function () {
 	        window.location.hash = target;
 	    });
 	});
+	$('a[href^="#experience"]').on('click',function (e) {
+		 e.preventDefault();
+
+		 var target = this.hash;
+		 var $target = $(target);
+
+		 $('html, body').stop().animate({
+			  'scrollTop': $target.offset().top
+		 }, 1100, 'swing', function () {
+			  window.location.hash = target;
+		 });
+	});
+	$('a[href^="#social"]').on('click',function (e) {
+		 e.preventDefault();
+
+		 var target = this.hash;
+		 var $target = $(target);
+
+		 $('html, body').stop().animate({
+			  'scrollTop': $target.offset().top
+		 }, 1100, 'swing', function () {
+			  window.location.hash = target;
+		 });
+	});
 });
+function navFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
